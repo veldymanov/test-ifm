c9a1587245cfcf3885d45bf0d27f36e04d9d492c
Veldymanov <veldymanov@gmail.com>
2018-02-28 13:43:53 +0200

    working game in chrome

--- src/app/pazzle/pazzle.component.ts
+++ src/app/pazzle/pazzle.component.ts
@@ -7,70 +7,118 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PazzleComponent implements OnInit {
 
-  private photoPieces: HTMLElement[] = [];
-  private photoPiecesZIndexes: string[] = [];
+  private dragablePieces: HTMLElement[] = [];
+  private dropTargets: HTMLElement[] = [];
+  private elemBelow: HTMLElement | undefined;
+  private elemBelowPieceN: string | undefined;
 
   constructor() {}
 
   ngOnInit() {
-    this.getPhotoPieces();
-    this.randomPhotoPieces();
-    this.photoPieces.forEach( (piece, index) => {
-      this.dragDrop(piece);
-      this.photoPiecesZIndexes[index] = piece.style.zIndex;
-    });
+    this.getDropTargets();
+    this.createDragablePieces();
   }
 
-  getPhotoPieces() {
-    for (let i = 0; i < 9; i++) {
-      this.photoPieces[i] = <HTMLElement>document.querySelector(`.js-piece-${i + 1}`);
+  getDropTargets() {
+    this.dropTargets = <HTMLElement[]>[].slice.call(document.querySelectorAll(`.js-drop-target`));
+
+  }
+
+  createDragablePieces() {
+    this.dragablePieces = <HTMLElement[]>[].slice.call(document.querySelectorAll(`.js-dragable-box`));
+    this.randomDragablePieces();
+
+    this.dragablePieces.forEach( (piece) => this.dragDrop(piece));
+  }
+
+  randomDragablePieces() {
+    const elWidth = this.dragablePieces[0].offsetWidth;
+    this.dragablePieces.forEach(piece => piece.style.cssText = randomizePieces());
+
+    function randomizePieces() {
+      return `
+        left: ${Math.floor( 2 * elWidth * Math.random() )}px;
+        top: ${ Math.floor( (250 - elWidth) * Math.random() )}px;
+        z-index: ${ Math.floor( 10 * Math.random() )};
+      `;
     }
   }
 
-  randomPhotoPieces() {
-    const elWidth = this.photoPieces[0].offsetWidth;
-    this.photoPieces.forEach(piece => piece.style.cssText = this.randomizePieces(elWidth));
-  }
-
-  randomizePieces(elWidth) {
-    return `
-      left: ${Math.floor( 2 * elWidth * Math.random() )}px;
-      top: ${ Math.floor( (250 - elWidth) * Math.random() )}px;
-      z-index: ${ Math.floor( 10 * Math.random() )};
-    `;
-  }
-
-  dragDrop(elem) {
-    elem.addEventListener('mousedown', (event: MouseEvent) => {
-      const style = window.getComputedStyle(elem);
+  dragDrop(dragableElem) {
+    dragableElem.addEventListener('mousedown', (event: MouseEvent) => {
+      const body = document.querySelector('body');
+      const style = window.getComputedStyle(dragableElem);
       const left0 = parseInt(style.getPropertyValue('left'), 10);
       const top0 = parseInt(style.getPropertyValue('top'), 10);
       const zIndex0 = style.getPropertyValue('z-index');
       const deltaX0 =  left0 - event.pageX;
       const deltaY0 = top0 - event.pageY;
 
-      document.onmousemove = function(e) {
-        e.preventDefault();
+      dragableElem.style.zIndex = 9999;
 
-        elem.style.cssText = `
-          left: ${e.pageX + deltaX0}px;
-          top: ${e.pageY + deltaY0}px;
-          z-index: 10;
-        `;
+      document.onmousemove = (e) => {
+        move(e);
+
+        // achieve element below dragable
+        dragableElem.style.visibility = 'hidden';
+        this.elemBelow = <HTMLElement>document.elementFromPoint(e.clientX, e.clientY);
+        dragableElem.style.visibility = 'visible';
+
+        this.elemBelowPieceN = this.elemBelow.dataset.piece;
+        // hover
+        this.unhoverAllDropTargets();
+        if (this.elemBelow.dataset.piece) {
+          this.elemBelow.style.backgroundColor = 'rgba(100, 100, 100, 0.5)';
+        }
       };
 
-      elem.onmouseup = function(e) {
-        e.preventDefault();
+      // prevent mouseleave the dragable element
+      body.onmouseleave = (e) => {
+        cancelMove(e);
+        removeMouseEvents();
+      };
 
-        elem.style.cssText = `
+      dragableElem.onmouseup = (e) => {
+
+        if (dragableElem.dataset.piece === this.elemBelowPieceN) {
+          this.elemBelow.appendChild(dragableElem);
+          console.log('dragableElem.dataset.piece: ', dragableElem.dataset.piece);
+        }
+
+        this.elemBelow = undefined;
+        cancelMove(e);
+        this.unhoverAllDropTargets();
+        removeMouseEvents();
+      };
+
+      function move(e) {
+        e.preventDefault();
+        dragableElem.style.cssText = `
+          left: ${e.pageX + deltaX0}px;
+          top: ${e.pageY + deltaY0}px;
+          z-index: 9999;
+        `;
+      }
+
+      function cancelMove(e) {
+        e.preventDefault();
+        dragableElem.style.cssText = `
           left: ${left0}px;
           top: ${top0}px;
           z-index: ${zIndex0};
         `;
+      }
 
+      function removeMouseEvents() {
+        body.onmouseleave = null;
         document.onmousemove = null;
-        elem.onmouseup = null;
-      };
+        dragableElem.onmouseup = null;
+      }
     });
   }
+
+  unhoverAllDropTargets() {
+      this.dropTargets.forEach(dropTarget => dropTarget.style.backgroundColor = 'rgba(100, 100, 100, 1)');
+    }
+
 }
