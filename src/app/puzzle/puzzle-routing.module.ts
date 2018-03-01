bc997824ee41ae62eb303330d987b4bf86b35a57
Veldymanov <veldymanov@gmail.com>
2018-03-01 10:32:22 +0200

    connecting to firebase

--- src/app/puzzle/puzzle-routing.module.ts
+++ src/app/puzzle/puzzle-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {
     path: 'puzzle',
     component: PuzzleComponent,
-  //  canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
